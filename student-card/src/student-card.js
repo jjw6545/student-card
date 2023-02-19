a3d93c8fcb46fd16e939156fd6c65c1091799a9f
jjw6545@psu.edu <122508759+Jw0001146366@users.noreply.github.com>
2023-02-19 11:07:12 -0500

    Update student-card.js

--- student-card/src/student-card.js
+++ student-card/src/student-card.js
@@ -1,86 +1,102 @@
 import { LitElement, html, css } from 'lit';
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+export class StudentCard extends LitElement {
+  static get styles () {
+    return css`
+       .buttons{
+   padding: 5px;
+   color: blue;
+}
 
-class StudentCard extends LitElement {
-  static properties = {
-    header: { type: String },
+.buttons:hover{
+  background-color: orange;
+}
+
+.buttons:focus {
+  background-color: orange; 
+}
+
+
+.all{
+  border-width: 5px;
+  width: 400px;
+  max-width: 500px;
+  margin: auto;
+  margin-top: 10px;
+}
+
+.title{
+  text-align: center;
+  font-size: 40px;
+}
+
+.name{
+  text-align: center;
+}
+.content{
+   text-align: center;
+}
+img {
+  width: 300px;
+}
+
+p {
+/*   text-align: left;  */
+  text-indent: 5%;
+  font-size: 18px;
+}
+
+.detailsButton {
+  margin: 12px;
+  position: center; 
+  color: blue;
+}
+
+  @media (min-width: 500px) and (max-width: 800px) {
+    
+    .detailsButton {
+      display: none;
+    }
+
+@media (max-width: 500px) {
+  .all{
+    transform: scale(0.8);
   }
-
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--student-card-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
+}
+    `;
+  };
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.version = 'STARTING';
   }
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
+    <button id="copy" class="buttons">Copy</button>
 
-        <p>Edit <code>src/StudentCard.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
+<button id="toggleBackGroundColor" class = "buttons">Change Background Color</button>
 
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+<button id="changeTitle" class = "buttons">Change Title</button>
+
+<button id="deleteButton" class = "buttons">Delete Last Instance</button>
+
+<section class="all">
+<header class ="name">
+  <h1 id = "mainTitle" class="title">Me When The Code Works</h1>
+  </header>
+   <p>
+My face when he code finaly decides to work.  This is Bryce.  He looks so cool.
+  </p>
+<section class="content">
+  <img src = "https://cdn.discordapp.com/attachments/1062806966021402684/1063160172773654659/IMG_9420.png">
+    
+    <button id = "detailsBtn" class="detailsButton">Details</button>
+    </a>
+  
+   </section>
+  
+</section>
     `;
   }
 }
